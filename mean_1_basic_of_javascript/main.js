8338019c99ed99e22958051848c7a714c7b5f9dd
ravi@ttn.com <booler@ttn.com>
2019-03-18 15:47:50 +0530

    Naming conventions changed

--- mean_1_basic_of_javascript/main.js
+++ mean_1_basic_of_javascript/main.js
@@ -1,24 +1,37 @@
-function startTime() {
-  var months = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sept','Oct','Nov','Dec'];
+var months = ['Jan','Feb','Mar','Apr','May','June','Jul','Aug','Sept','Oct','Nov','Dec'];
   var today = new Date();
   var h = today.getHours();
-  var m = today.getMinutes();
-  var s = today.getSeconds();
+  var minute = today.getMinutes();
+  var second = today.getSeconds();
   var ampm = "AM";
   var date = today.getDate();
   var month = today.getMonth();
   var year = today.getYear();
-  m = checkTime(m);
-  s = checkTime(s);
   var greet = "";
-  if (h>=4 && h<11) {greet = "Morning";}
-  if (h>=11 && h<16) {greet = "Afternoon";}
-  if (h>=16 && h<20) {greet = "Evening";}
-  if ((h>=20 && h<24) || (h<4)) {greet = "Night";}
-  if (h>12) {h-=12; ampm = "PM"}
-  h=checkTime(h);
-  document.getElementById('time').innerHTML = h + ":" + m+ ":" + s + " " + ampm;
-  document.getElementById('date').innerHTML = date+" "+months[month]+" "+(year+1900);
+
+function startTime() {
+  var flag = 0;
+  today = new Date();
+  hour = today.getHours();
+  minute = today.getMinutes();
+  second = today.getSeconds();
+  ampm = "AM";
+  minute = checkTime(minute);
+  second = checkTime(second);
+  var greet = "";
+  if (hour>=4 && hour<11) {greet = "Morning";}
+  if (hour>=11 && hour<16) {greet = "Afternoon";}
+  if (hour>=16 && hour<20) {greet = "Evening";}
+  if ((hour>=20 && hour<24) || (hour<4)) {greet = "Night";}
+  if (hour>12) {hour-=12; ampm = "PM"}
+  hour=checkTime(hour);
+  document.getElementById('time').innerHTML = hour + ":" + minute+ ":" + second + " " + ampm;
+  if(date != today.getDate()) {date = today.getDate(); flag = 1;}
+  if(month != today.getMonth()) {month = today.getMonth(); flag = 1;}
+  if(year != today.getYear()) {year = today.getYear(); flag = 1;}
+  if(flag == 1){
+    document.getElementById('date').innerHTML = date+" "+months[month]+" "+(year+1900);
+  }
   document.getElementById('greeting').innerHTML = "Good "+greet;
   var t = setTimeout(startTime, 1000);
 }
